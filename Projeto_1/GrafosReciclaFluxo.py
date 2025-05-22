7f93ef033be0d00216ee9fed2f2948299e5e74e5
KaduRosendo <100209440+KaduRosendo@users.noreply.github.com>
2025-05-22 19:45:40 -0300

    Update GrafosReciclaFluxo.py

--- Projeto_1/GrafosReciclaFluxo.py
+++ Projeto_1/GrafosReciclaFluxo.py
@@ -3,7 +3,6 @@
 # Matheus Santiago de Brito -       10408953
 # Carlos Eduardo Rosendo Basseto -  10409941
 # Luiz Henrique Ribeiro Pulga -     10409246
-# Isabella Rodrigues de Oliveira -  10357696
 
 import re
 import sys
